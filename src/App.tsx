f81217ce0218bb101d1526f4f522f7f6b3f0b552
adityaprasad-48 <adityaprasad20799@gmail.com>
2021-06-28 21:40:24 +0530

    new-changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
-import { Counter as CounterOne } from './counter/Counter';
-import GitTestFile from './counter/GitTestFile';
-import Info from './information/Info';
+import React, { useState } from "react";
+import "./App.css";
+import GitTestFile from "./counter/GitTestFile";
+import Info from "./information/Info";
 
 function App() {
-	const [name, setName] = useState('Aditya');
-	return (
-		<div className="App">
-			<header className="App-header">
-				<div className="one_two_ka_four">
-					<CounterOne  />
-				</div>
-				<GitTestFile name={name} />
-			</header>
-			<section className="one_two_ka_four">
-				<Info />
-			</section>
-		</div>
-	);
+  const [name, setName] = useState("Aditya");
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GitTestFile name={name} />
+      </header>
+      <section className="one_two_ka_four">
+        <Info />
+      </section>
+    </div>
+  );
 }
 
 export default App;
