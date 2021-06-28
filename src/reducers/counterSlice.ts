dd6695720b832edfa849179010dedc35ed84054e
adityaprasad-48 <adityaprasad20799@gmail.com>
2021-06-28 21:44:11 +0530

    nnkk

--- src/reducers/counterSlice.ts
+++ src/reducers/counterSlice.ts
@@ -21,7 +21,84 @@ export const counterSlice = createSlice({
       state.value -= 1;
     },
     // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmountSpecified: (state, action: PayloadAction<number>) => {
+    incrementByAmountSpecified: (state, 
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			action: PayloadAction<number>) => {
       state.value += action.payload;
     },
   },
@@ -29,7 +106,54 @@ export const counterSlice = createSlice({
 
 export const selectCount = (state: RootState) => state.counter.value;
 
-export const { increment, decrement, incrementByAmountSpecified } =
+export const { increment, 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	decrement, 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	incrementByAmountSpecified } =
   counterSlice.actions;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 
 export default counterSlice.reducer;
