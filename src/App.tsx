e825a4f9f877fd4164f3dcfcb785cd4b6c95807f
adityaprasad-48 <adityaprasad20799@gmail.com>
2021-07-05 09:24:26 +0530

    overflow fixed

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-	font-family: 'Poppins', sans-serif;
+	  font-family: 'Poppins', sans-serif;
   }
 `;
 
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -35,10 +35,14 @@ const NavBar = () => {
 	useEffect(() => {
 		if (elementPosition > -1) {
 			setOverlay(true);
+			document.body.style.overflow = 'hidden';
 		} else {
 			setOverlay(false);
+			document.body.style.overflow = '';
 		}
 	}, [elementPosition]);
+
+	// Add Framer Motion in submene opening
 
 	return (
 		<Header>
@@ -53,7 +57,7 @@ const NavBar = () => {
 				<ProfileDiv>
 					<SettingNavDropDown
 						elementPosition={6}
-						title="Dashboard"
+						title="Settings"
 						opened={elementPosition === 6}
 						onHover={onHover}
 					/>
@@ -76,6 +80,7 @@ const NavBar = () => {
 export default NavBar;
 
 // backdrop could be just overlay to show something is opened
+// hover highlight setting icon
 
 const NavButtons = ({ onHover, elementPosition }: any) => {
 	return (
@@ -130,9 +135,12 @@ const NavDropDown = ({ title, opened, elementPosition, onHover }: any) => {
 				{title}
 			</DropDownBtn>
 			<DropDownContent active={opened}>
-				<h1>{title}</h1>
-				<h1>{title}</h1>
-				<h1>{title}</h1>
+				<div className="dropdown-content">
+					<h1 style={{ border: '1px solid red' }}>{title}</h1>
+					<h1>{title}</h1>
+					<h1>{title}</h1>
+					<p>New here Welcome to natty hatty</p>
+				</div>
 			</DropDownContent>
 		</DropDown>
 	);
@@ -145,7 +153,14 @@ const SettingNavDropDown = ({
 	onHover,
 }: any) => {
 	return (
-		<DropDown onMouseLeave={() => onHover(-1)}>
+		<div
+			style={{
+				height: '100%',
+				display: 'flex',
+				alignItems: 'center',
+			}}
+			onMouseLeave={() => onHover(-1)}
+		>
 			<IconBtn onMouseEnter={() => onHover(elementPosition)} type="button">
 				<SettingsIcon fontSize="large" />
 			</IconBtn>
@@ -154,6 +169,6 @@ const SettingNavDropDown = ({
 				<h1>{title}</h1>
 				<h1>{title}</h1>
 			</DropDownContent>
-		</DropDown>
+		</div>
 	);
 };
